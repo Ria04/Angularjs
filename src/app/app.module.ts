40137cb98e9adbf4cf78b76d09673a706e4610b8
RiYo <you@example.com>
2020-04-30 16:20:43 +0530

    make it better

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule }   from '@angular/forms';
+
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Ng2SearchPipeModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,103 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+  x:string;
+
+  movies = [
+    {
+	moviename: 'This the Season for Love',
+    },
+    {
+  moviename: '17',
+    },
+    {
+  moviename: '1 Night',
+    },
+       {
+	moviename: '100 days',
+    },
+    {
+  moviename: '1000 Days',
+    },
+    {
+  moviename: 'Election',
+    },
+     {
+	moviename: 'Hum aapke hai kon',
+    },
+    {
+  moviename: '3 idiots',
+    },
+    {
+  moviename: 'Asur',
+    },
+    {
+	moviename: 'Don',
+    },
+    {
+  moviename: 'Sean BeanBean series',
+    },
+    {
+  moviename: 'war',
+    },
+    {
+  moviename: 'agneepat',
+    },
+    {
+	moviename: 'jab we met',
+    },
+    {
+  moviename: 'hello mini',
+    },
+    {
+  moviename: 'sacred games',
+    },
+       {
+	moviename: '17 days',
+    },
+    {
+  moviename: 'ishqbaaz',
+    },
+    {
+  moviename: 'yeh javani nai deevani',
+    },
+     {
+	moviename: 'hum saath saath hai',
+    },
+    {
+  moviename: 'fanna',
+    },
+    {
+  moviename: 'stranger things',
+    },
+    {
+	moviename: 'Don 2',
+    },
+    {
+  moviename: 'mr bean',
+    },
+    {
+  moviename: 'chota bheem',
+    },
+    {
+  moviename: 'tom and jerry',
+    }
+  ];
+  constructor() { 
+
+    
+  }
+ 
+  ngOnInit(): void {
+  }
+  
+
+  
+  }
+
