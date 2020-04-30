c8822456735707f0a42e1df2706452b11d78cb99
RiYo <you@example.com>
2020-04-30 17:41:42 +0530

    my first commit

--- nodeapp/server.js
+++ nodeapp/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/nodeapp'));
 
 // Send all requests to index.html
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/nodeapp/index.html'));
 });
 
